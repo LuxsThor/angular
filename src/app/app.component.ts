102e8f71d1b68af4cf0a3876929077e400be8aa3
LuxsThor <lucas.thorbecke@bluewin.ch>
2019-02-21 18:33:34 +0100

    separated into compnents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import {TodoDataService} from './todo-data.service';
+import {Todo} from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
+
+
+
 export class AppComponent {
-  title = 'toDo';
+
+  newTodo: Todo = new Todo
+
+
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  // Add new method to handle event emitted by TodoListHeaderComponent
+  onAddTodo(todo: Todo) {
+    this.todoDataService.addTodo(todo);
+  }
+
+  // rename from toggleTodoComplete
+ onToggleTodoComplete(todo: Todo) {
+   this.todoDataService.toggleTodoComplete(todo);
+ }
+
+ // rename from removeTodo
+ onRemoveTodo(todo: Todo) {
+   this.todoDataService.deleteTodoById(todo.id);
+ }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
 }
